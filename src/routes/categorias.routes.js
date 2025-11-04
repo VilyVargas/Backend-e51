65473b2f0cd77b688bd72ec27208000d2485d398
VilyVargas <vily05dominic23@gmail.com>
2025-11-04 10:33:33 -0600

    masss

--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { eliminarCategoria, obtenerCategorias, actualizarCategoriaPatch, registrarCategoria, obtenerCategoria} from "../controllers/categorias.controller.js";
-
+import {
+  eliminarCategoria,
+  obtenerCategorias,
+  actualizarCategoriaPatch,
+  registrarCategoria,
+  obtenerCategoria,
+} from "../controllers/categorias.controller.js";
 
 const router = Router();
-
 
 // Ruta para obtener todos los clientes
 router.get("/categorias", obtenerCategorias);
@@ -11,11 +15,13 @@ router.get("/categorias", obtenerCategorias);
 router.delete("/eliminarcategorias/:id_categoria", eliminarCategoria);
 
 // Ruta para actualizar una categoría por su ID
-router.patch('/actualizarcategoria/:id_categoria', actualizarCategoriaPatch);
+router.patch("/actualizarcategoria/:id_categoria", actualizarCategoriaPatch);
 
 // Ruta para registrar una nueva Categoría
-router.post('/registrarcategoria', registrarCategoria);
+router.post("/registrarcategoria", registrarCategoria);
 
+
+//nashdajshdjashdjas
 // Ruta para obtenr una categoria por su ID
 router.get("/categoria/:id_categorias", obtenerCategoria);
 
